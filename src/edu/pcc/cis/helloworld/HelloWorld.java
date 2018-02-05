b2f4fa235cfd5567e0b27f8bdf75a97f65efb98d
Maryam Khan <maryam.khan@pcc.edu>
2018-02-04 21:25:59 -0800

    added main method that prints hello world

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -4,4 +4,7 @@ package edu.pcc.cis.helloworld;
  * created by Maryam Khan on 2/4/2018
  */
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
