b5aaaecb4a15ef619519c14e5318c5e81e10b405
Maryam Khan <maryam.khan@pcc.edu>
2018-02-04 21:31:18 -0800

    print date

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,10 +1,14 @@
 package edu.pcc.cis.helloworld;
 
+import java.util.Date;
+
 /**
  * created by Maryam Khan on 2/4/2018
  */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println(new Date());
+        System.out.println("by Maryam Khan");
     }
 }
